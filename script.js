e7c56c75eedfb83c1794332ddf68c5b7422888a2
Bee <xronsredstone@gmail.com>
2022-11-03 18:53:12 -0400

    actual revert

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-if (!process.argv[2]) {var div=0};
-const dig = process.argv[2]
-if (dig>0) {calcFibServer()}else{
-  const generateBtn = document.getElementById('generate');
-  generateBtn.addEventListener("click", calcFib);}
-function calcFibServer(){
-  const fibby = [];
-  const fibonacci = (position) => {
-    if (position < 2) {
-      return position;}
-    return fibonacci(position - 1) + fibonacci(position - 2);};
-  const fibonacciUpTo = (value) => {
-    if (value > 0) {
-      fibby.push(fibonacci(value));
-      return fibonacciUpTo(value - 1);
-    } else {
-      const reversed = fibby.reverse();
-      console.log(`[${reversed}]`)
-      return value;}};
-  fibonacciUpTo(dig);}
+const generateBtn = document.getElementById('generate');
+generateBtn.addEventListener("click", calcFib);
 function calcFib(){
-  const diggy = prompt("How many digits do you want to calculate to?");
+  const dig = prompt("How many digits do you want to calculate to?");
   const fibby = [];
   const fibonacci = (position) => {
     if (position < 2) {
@@ -36,4 +18,5 @@ function calcFib(){
       generateBtn.setAttribute("style", "background-color: rgb(92, 212, 76);", );
       document.getElementById("generate").innerHTML = "Generate another array";
       return value;}};
-  fibonacciUpTo(diggy);}
+  fibonacciUpTo(dig);
+}
