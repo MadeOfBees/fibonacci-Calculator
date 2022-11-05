550620568569d6354ed4e7d15bb97c0e80f82962
Bee <xronsredstone@gmail.com>
2022-11-04 21:57:18 -0400

    BIG INT

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const generateBtn = document.getElementById('generate');
 generateBtn.addEventListener("click", calcFib);
-function calcFib(){
+function calcFib() {
   const dig = prompt("How many digits do you want to calculate to?");
   const fibby = [];
   const fibonacci = (position) => {
     if (position < 2) {
-      return position;}
-      var sqRootOf5 = Math.sqrt(5);
-      var var1 = (1+sqRootOf5)/2;
-      var var2 = (1-sqRootOf5)/2
-      return Math.round((Math.pow(var1, position) - Math.pow(var2, position)) / sqRootOf5);};
+      return position;
+    };
+    const sqRootOf5 = Math.sqrt(5);
+    let v1 = (1 + sqRootOf5) / 2;
+    let v2 = (1 - sqRootOf5) / 2;
+    return BigInt(Math.round((Math.pow(v1, position) - Math.pow(v2, position)) / sqRootOf5));
+  };
   const fibonacciUpTo = (value) => {
     if (value > 0) {
       fibby.push(fibonacci(value));
@@ -18,8 +20,10 @@ function calcFib(){
       const reversed = fibby.reverse();
       const printText = document.querySelector("#screenText");
       printText.value = `[${reversed}]`;
-      generateBtn.setAttribute("style", "background-color: rgb(92, 212, 76);", );
+      generateBtn.setAttribute("style", "background-color: rgb(92, 212, 76);",);
       document.getElementById("generate").innerHTML = "Generate another array";
-      return value;}};
+      return value;
+    }
+  };
   fibonacciUpTo(dig);
 }
