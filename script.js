ee0e6037e99e3ea24333abc98a4f7d6156085e21
Bee <xronsredstone@gmail.com>
2022-11-08 00:29:56 -0500

    spaced

--- script.js
+++ script.js
@@ -3,18 +3,22 @@ document.getElementById('generate').addEventListener("click", function () {
   const fibby = [];
   const fibonacci = (position) => {
     if (position < 2) return position;
-    return Math.round((Math.pow((1 + Math.sqrt(5)) / 2, position) - Math.pow((1 - Math.sqrt(5)) / 2, position)) / Math.sqrt(5));};
+    return Math.round((Math.pow((1 + Math.sqrt(5)) / 2, position) - Math.pow((1 - Math.sqrt(5)) / 2, position)) / Math.sqrt(5));
+  };
   const fibonacciUpTo = (value) => {
-    if (value > 70 || value < -1 || !Number.isInteger(parseInt(value))){
+    if (value > 70 || value < -1 || !Number.isInteger(parseInt(value))) {
       alert(`Value can only be a real whole number between 1 and 70`)
       return
-    }else{
+    } else {
       if (value > 0) {
         fibby.push(fibonacci(value));
         return fibonacciUpTo(value - 1);
-      }else{
+      } else {
         document.querySelector("#screenText").value = `[` + fibby.reverse() + `]`;
         document.getElementById('generate').setAttribute("style", "background-color: rgb(92, 212, 76);",).innerHTML = "Generate another array";
-        return value;};};};
-    fibonacciUpTo(dig)
+        return value;
+      };
+    };
+  };
+  fibonacciUpTo(dig)
 });
